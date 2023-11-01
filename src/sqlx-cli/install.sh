4c308944d61243bfa9ff2dce8c9d6726ed5de917
TiamoP23 <43099774+TiamoP23@users.noreply.github.com>
2023-11-01 00:32:25 +0000

    test

--- src/sqlx-cli/install.sh
+++ src/sqlx-cli/install.sh
@@ -1,7 +1,2 @@
-echo "The effective dev container remoteUser is '$_REMOTE_USER'"
-echo "The effective dev container remoteUser's home directory is '$_REMOTE_USER_HOME'"
-
-echo "The effective dev container containerUser is '$_CONTAINER_USER'"
-echo "The effective dev container containerUser's home directory is '$_CONTAINER_USER_HOME'"
-
-su -p $_REMOTE_USER -c "cargo install sqlx-cli"
+su $_REMOTE_USER
+cargo install sqlx-cli
